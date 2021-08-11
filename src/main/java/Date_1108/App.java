9285d1fe9771a030d51603d7ac58be5bf8bf983f
ZaynPhan <88411710+ZaynPhan@users.noreply.github.com>
2021-08-11 23:37:55 +0700

    Date 11/08

--- src/main/java/Date_1108/App.java
+++ src/main/java/Date_1108/App.java
@@ -5,50 +5,51 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-    //2. Các thao tác kiểm tra
-    //b. Mảng có phải là mảng toàn số nguyên tố?
+        //2. Các thao tác kiểm tra
+        //b. Mảng có phải là mảng toàn số nguyên tố?
         System.out.println("==========Exercise 2B==========");
         Scanner input = new Scanner(System.in);
         System.out.print("Nhập độ dài mảng: ");
         int n = input.nextInt();
         int[] arr2B = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= (n-1) ; i++) {
             System.out.format("a[%d] = ", i);
             arr2B[i] = input.nextInt();
         }
         Exercise1108.printArray(arr2B);
 
-        if(Exercise1108.isPrimeNumber(arr2B)){
+        boolean checkPrimeArr = Exercise1108.isPrimeArr(arr2B);
+        if (checkPrimeArr) {
             System.out.println("Là mảng chứa toàn số nguyên tố");
-        }else{
+        } else {
             System.out.println("KHÔNG PHẢI là mảng chứa toàn số nguyên tố");
         }
 
-    //2. Các thao tác kiểm tra
-    //c. Mảng có phải là mảng tăng dần?
+        //2. Các thao tác kiểm tra
+        //c. Mảng có phải là mảng tăng dần?
         System.out.println("==========Exercise 2C==========");
         System.out.print("Nhập độ dài mảng: ");
         int l = input.nextInt();
         int[] arr2C = new int[l];
-        for (int i = 0; i < l; i++) {
+        for (int i = 0; i <= (l-1); i++) {
             System.out.format("a[%d] = ", i);
             arr2C[i] = input.nextInt();
         }
         Exercise1108.printArray(arr2C);
 
-        if(Exercise1108.isAscending(arr2C)){
+        if (Exercise1108.isAscending(arr2C)) {
             System.out.println("Là mảng tăng dần");
-        }else{
+        } else {
             System.out.println("KHÔNG PHẢI Là mảng tăng dần");
         }
 
     //3. Các thao tác tính toán
-    // a. Có bao nhiêu số chia hết cho 4 nhưng không chia hết cho 5
+    //a. Có bao nhiêu số chia hết cho 4 nhưng không chia hết cho 5
         System.out.println("==========Exercise 3A==========");
         System.out.print("Nhập độ dài mảng: ");
         int m = input.nextInt();
         int[] arr3A = new int[m];
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i <= (m-1); i++) {
             System.out.format("a[%d] = ", i);
             arr3A[i] = input.nextInt();
         }
@@ -57,12 +58,12 @@ public class App {
         System.out.format("Có %d số chia hết cho 4 nhưng không chia hết cho 5", Exercise1108.module4Not5(arr3A));
         System.out.println();
 
-    //b. Tổng các số nguyên tố có trong mảng
+        //b. Tổng các số nguyên tố có trong mảng
         System.out.println("==========Exercise 3B==========");
         System.out.print("Nhập độ dài mảng: ");
         int o = input.nextInt();
         int[] arr3B = new int[o];
-        for (int i = 0; i < o; i++) {
+        for (int i = 0; i <= (o-1) ; i++) {
             System.out.format("a[%d] = ", i);
             arr3B[i] = input.nextInt();
         }
@@ -70,3 +71,4 @@ public class App {
         System.out.format("Tổng các số nguyên tố trong mảng 3B là: %d", Exercise1108.sumPrimeNumber(arr3B));
     }
 }
+
--- src/main/java/Date_1108/Exercise1108.java
+++ src/main/java/Date_1108/Exercise1108.java
@@ -1,22 +1,23 @@
 package Date_1108;
+
 import Date_0908.exercise0906;
 
 
 public class Exercise1108 {
     public static void printArray(int[] arr) {
-        for (int i : arr) {
+        for (int i = 0; i <= (arr.length - 1); i++) {
             System.out.format("%d, ", arr[i]);
         }
         System.out.println();
     }
 
-    public static boolean isPrimeNumber(int[] arr2B) {
-        for (int n : arr2B) {
-            if (n < 2) {
+    public static boolean isPrimeArr(int[] arr2B) {
+        for (int i = 0; i <= (arr2B.length - 1); i++) {
+            if (arr2B[i] < 2) {
                 return false;
             } else {
-                for (int i = 2; i <= Math.sqrt(n); i++) {
-                    if (n % i == 0) {
+                for (int j = 2; j <= Math.sqrt(arr2B[j]); j++) {
+                    if (arr2B[j] % j == 0) {
                         return false;
                     }
                 }
@@ -25,16 +26,16 @@ public class Exercise1108 {
         return true;
     }
 
-    public static boolean isAscending(int[] arr2C){
-        for (int i = 0; i <= arr2C.length - 1; i++) {
-            if (arr2C[i] >= arr2C[i+1]){
+    public static boolean isAscending(int[] arr2C) {
+        for (int i = 0; i <= (arr2C.length - 1); i++) {
+            if (arr2C[i] >= arr2C[i + 1]) {
                 return false;
             }
         }
         return true;
     }
 
-    public static int module4Not5(int[] arr3A){
+    public static int module4Not5(int[] arr3A) {
         int amount = 0;
         for (int j : arr3A) {
             if ((j % 4 == 0) && (j % 5 != 0)) {
@@ -44,7 +45,7 @@ public class Exercise1108 {
         return amount;
     }
 
-    public static int sumPrimeNumber(int[] arr3B){
+    public static int sumPrimeNumber(int[] arr3B) {
         int sumPrime = 0;
         for (int i : arr3B) {
             if (exercise0906.isPrimeNumber(i)) {
