80d32a41f698baef1c5428e83afe5b67a920b638
ZaynPhan <88411710+ZaynPhan@users.noreply.github.com>
2021-10-05 07:51:22 +0700

    Date 28/09 (Done)

--- src/main/java/Date_0210/App.java
+++ src/main/java/Date_0210/App.java
@@ -1,11 +1,30 @@
 package Date_0210;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+//Bài tập: viết chương trình Java cho phép copy tập tin bất kỳ trong máy tính,
+// sử dụng phương pháp `đọc file nguồn => ghi file đích`
 
 public class App {
     public static void main(String[] args) {
 
+        try {
+            FileInputStream fis = new FileInputStream("RadicalFile.bin");
+            BufferedInputStream bis = new BufferedInputStream(fis);
 
+            FileOutputStream fos = new FileOutputStream("CopyFile.bin");
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            int text = bis.read();
+            while (text != -1) {
+                bos.write(text);
+                text = bis.read();
+            }
+
+            bos.flush();
+            bos.close();
+            bis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
